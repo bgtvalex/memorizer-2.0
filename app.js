099023932af42bf483e107870fe308ef9e3a36b4
Alexander Bogatenkov <33396051+bgtvalex@users.noreply.github.com>
2025-04-01 21:43:29 +0300

    app path without /

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import { init } from '/js/func/init.js'
-import { closeModal } from '/js/elements/modal/modal.js'
-import { addCard } from '/js/elements/modal/add-card.js'
-import { getVersion } from '/js/versions/get-version.js'
+import { init } from 'js/func/init.js'
+import { closeModal } from 'js/elements/modal/modal.js'
+import { addCard } from 'js/elements/modal/add-card.js'
+import { getVersion } from 'js/versions/get-version.js'
 
 init()
 getVersion()
