bac6e8b2cfa3be7f90be08ef1807e5d47e28bcd7
Alexander Bogatenkov <33396051+bgtvalex@users.noreply.github.com>
2025-04-01 22:14:31 +0300

    update 8

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { init } from './js/func/init.js'
 import { closeModal } from './js/elements/modal/modal.js'
 import { addCard } from './js/elements/modal/add-card.js'
 import { getVersion } from './js/versions/get-version.js'
-alert('init')
+
 init()
 getVersion()
 
